d6fc83ae84ed38c848827addca332e7084679d81
Igor Todorovski <itodorov@ca.ibm.com>
2019-04-29 15:05:41 -0400

    Update setup.sh script

--- setup.sh
+++ setup.sh
@@ -2,15 +2,13 @@
 # This script will set Node to be program controlled in order to access RACF
 
 if [ -z $NODE_INSTALL_DIR ]; then
-	echo "\$NODE_INSTALL_DIR must be set to the base install dir of NodeJS for z/OS"
-	exit 1
+	NODE_INSTALL_DIR=`dirname $(/bin/type node | /bin/sed -e "s/node is //g")`/..
 fi
 
 set -x
-extattr +p build/Release/racf.node
-if [ -e "build/Release/racf.node"  ]; then
-	extattr +p "build/Release/racf.node"
-fi
+
+find . -name "racf.node" -exec extattr +p  {} \;
+
 if [ -e "$NODE_INSTALL_DIR/lib/libnode.so" ]; then
 	extattr +p "$NODE_INSTALL_DIR/lib/libnode.so"
 fi
